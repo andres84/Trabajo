5108ba09f675dd982da7ab0024952e42eba6c739
SARA <SARA@SARA-PC>
2016-01-27 17:18:34 -0500

    4 Modificacion Trabajo1

--- Trabajo1/src/trabajo1/Trabajo1.java
+++ Trabajo1/src/trabajo1/Trabajo1.java
@@ -15,6 +15,7 @@ public class Trabajo1 {
     String nombre;
     int valor;
     double dato;
+    int a;
     
     public static void main(String[] args) {
         // TODO code application logic here
